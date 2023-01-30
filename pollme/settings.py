80b1326f2ed23966ba808eb2ca93ac7f63beaa9a
Radee1 <roseadee2013@gmail.com>
2023-01-30 11:31:39 +0000

    Turned off debugging in production branch

--- pollme/settings.py
+++ pollme/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-if0zdcz-hv8os)8f)y=^jab@ljb%kkwj_t5h(%a$g87pxw2^sk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
