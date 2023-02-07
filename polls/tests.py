a28e10b52aa691a0a6564b73c150510367db04e4
Radee1 <roseadee2013@gmail.com>
2023-02-07 07:18:20 +0000

    Added URL testing to accounts

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,13 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from .models import Poll
 
-# Create your tests here.
+class UrlTest(TestCase):
+
+    def testPollsPage(self):
+        response = self.client.get('/polls')
+        print(response)
+
+        self.assertEqual(response.status_code, 200)
+
