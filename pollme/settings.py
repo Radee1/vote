8ae9ea188e19fc19067d3c5ffc6f9ad73761f104
Radee1 <roseadee2013@gmail.com>
2023-02-07 14:06:14 +0000

    Added database url

--- pollme/settings.py
+++ pollme/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -106,11 +107,15 @@ WSGI_APPLICATION = 'pollme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://fceamupf:qeXs_6oWCK4j1L6BNhFHiKtWQ2m1ZqLx@snuffleupagus.db.elephantsql.com/fceamupf')
 }
 
 
