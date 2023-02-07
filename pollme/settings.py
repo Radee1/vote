87524a949f247fe99cd95d86b796c0ffed965d7b
Radee1 <roseadee2013@gmail.com>
2023-02-07 18:32:27 +0000

    Removed keys from settings.py

--- pollme/settings.py
+++ pollme/settings.py
@@ -113,17 +113,18 @@ WSGI_APPLICATION = 'pollme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+if 'DATABASE_URL' in os.environ:
 
-DATABASES = {
-    'default': dj_database_url.parse.env('DATABASE_NAME')
-}
-
+    DATABASES = {
+        'default': dj_database_url.parse.env('DATABASE_NAME')
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
