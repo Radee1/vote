1db3bed46fec8e7844ece5f835aa4af21a92ad6c
Radee1 <roseadee2013@gmail.com>
2023-03-16 18:59:58 +0000

    ADDED CHANGES TO PASS PYCODESTYLE VALIDATOR CHECK

--- polls/tests.py
+++ polls/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 from .models import Poll
 
+
 class ModelTest(TestCase):
 
     def testPollModel(self):
-        poll = Poll.objects.create(owner="Dada1",text="Test Text" )
+        poll = Poll.objects.create(owner="Dada1", text="Test Text")
         self.assertEquals(str(poll), 'Test_Poll')
-        print("IsInstance : ",isinstance(poll,Poll))
-        self.assertTrue(isinstance(poll,Poll))
+        print("IsInstance : ", isinstance(poll, Poll))
+        self.assertTrue(isinstance(poll, Poll))
