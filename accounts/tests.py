fb440c81e890f945ad0c544dbb114d1988fa0ed3
Radee1 <roseadee2013@gmail.com>
2023-03-18 16:11:06 +0000

    ADDED URL TESTS
    Added url unit tests to main branch.

--- accounts/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-class UrlTest(TestCase):
-
-    def testHomePage(self):
-        response = self.client.get('/')
-        print(response)
-
-        self.assertEqual(response.status_code, 200)
--- polls/tests.py
+++ polls/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .models import Poll
+#from .models import Poll, Choice, Vote
+from django.urls import reverse
+
 
 class ModelTest(TestCase):
 
@@ -8,3 +11,41 @@ class ModelTest(TestCase):
         self.assertEquals(str(poll), 'Test_Poll')
         print("IsInstance : ",isinstance(poll,Poll))
         self.assertTrue(isinstance(poll,Poll))
+
+
+class IndexTest(TestCase):
+
+    def testIndexPage(self):
+        response = self.client.get('/')
+        print(response)
+        self.assertEqual(response.status_code, 200)
+
+
+class LoginTest(TestCase):
+
+    def testLoginPage(self):
+        response = self.client.get('accounts:login')
+        print(response)
+
+
+class SignupTest(TestCase):
+
+    def testSignUpPage(self):
+        response = self.client.get('accounts:register')
+        print(response)
+
+
+class NotFoundTest(TestCase):
+
+    def test404Page(self):
+        response = self.client.get('accounts:registerhk')
+        print(response)
+        self.assertEqual(response.status_code, 200)
+
+
+'''
+class ModelTestCase(TestCase):
+    def setUp(self):
+        Diagnosis.objects.create(patient_name="John Doe", test_name="Malaria Test", diagnosis="Malaria Positive")
+        Diagnosis.objects.create(patient_name="Jane Din", test_name="Sputum test", diagnosis="Covid Positive")
+'''
